f6e04a91e138412b3a0a3316d29e5d5b58f07086
treblab <albertnguyen2002@hotmail.com>
2024-03-28 15:22:20 +1100

    Removing old movement scripts

--- 2 birds movement/Assets/Scripts/PlayerController.cs
+++ 2 birds movement/Assets/Scripts/PlayerController.cs
@@ -1,87 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using static Unity.Burst.Intrinsics.Arm;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] GameObject pigeon;
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
 
-    private Tweener tweener;
-    private char currentInput;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
-    // Map of keys and their corresponding directions
-    private Dictionary<char, Vector2> charToDirection = new Dictionary<char, Vector2>
-    {
-        { 'W', Vector2.up },
-        { 'A', Vector2.left },
-        { 'S', Vector2.down },
-        { 'D', Vector2.right }
-    };
-
-    // To record KeyCodes as chars
-    private Dictionary<KeyCode, char> keyToChar = new Dictionary<KeyCode, char>
-    {
-        { KeyCode.W, 'W' },
-        { KeyCode.A, 'A' },
-        { KeyCode.S, 'S' },
-        { KeyCode.D, 'D' }
-    };
-
-    // To rotate air particles based on movement - can be used later
-    private Dictionary<Vector2, float> directionToRotation = new Dictionary<Vector2, float>
-    {
-        { Vector2.up, 0f },
-        { Vector2.down, 180f },
-        { Vector2.left, 90f },
-        { Vector2.right, -90f }
-    };
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        tweener = GetComponent<Tweener>();
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        getPlayerInput();
-        // movePigeon();
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
     }
 
-    private void getPlayerInput()
+    private void FixedUpdate()
     {
-        if (pigeon == null) return;
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
 
-        foreach (var key in keyToChar.Keys)
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
-            if (Input.GetKeyDown(key))
-            {
-                currentInput = keyToChar[key];
-                movePigeon();
-            }
-            if (Input.GetKeyUp(key)) return;
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
         }
     }
-
-
-    private void movePigeon()
-    {
-        if (pigeon == null || !charToDirection.ContainsKey(currentInput)) return;
-
-        Vector2 direction = charToDirection[currentInput];
-
-        // float moveSpeed = 5f; // Customize this speed as needed
-        // Debug.Log("Move amount: " + moveAmount);
-
-        // Calculating the new position based on the direction
-        Vector3 newPosition = pigeon.transform.position + new Vector3(direction.x, direction.y, 0);
-        Debug.Log("Previous pos: " + pigeon.transform.position);
-        pigeon.transform.position = newPosition;
-        Debug.Log("New pos: " + pigeon.transform.position);
-    }
-
 }
