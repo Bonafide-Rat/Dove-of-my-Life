62eeb4fefa82768294b5d3f67f5df9b7036aefc9
treblab <albertnguyen2002@hotmail.com>
2024-03-28 16:47:06 +1100

    Initial implementation of flutter jump

--- 2 birds movement/Assets/Scripts/PlayerController.cs
+++ 2 birds movement/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,14 @@ public class PlayerController : MonoBehaviour
 {
     private float horizontal;
     private float speed = 8f;
-    private float jumpingPower = 16f;
+    private float jumpingPower = 8f;
     private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+
+    private bool flutterUsed = false;
 
     void Update()
     {
@@ -18,15 +20,29 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            Debug.Log("Spacebar pressed");
+            flutterUsed = false;
         }
 
+        else if (Input.GetButtonDown("Jump") && !IsGrounded() && !flutterUsed)
+        {
+            rb.velocity = new Vector2 (rb.velocity.x, jumpingPower * 0.25f);
+            Debug.Log("Flutter jump triggered");
+            flutterUsed = true;
+        }
+
+        /*
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            Debug.Log("Spacebar up");
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.25f);
+            Debug.Log("Spacebar up, velocity: " + rb.velocity);
         }
+        */
+
 
         Flip();
-    }
+    }  
 
     private void FixedUpdate()
     {
