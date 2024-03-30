2869b42a2cd6487a1fc5f599e7a8f428a1de935f
treblab <albertnguyen2002@hotmail.com>
2024-03-30 21:18:41 +1100

    Coyote Time Implemented
    First implementation of coyote time, try to simplify and make code more readable.

--- 2 birds movement/Assets/Scripts/PlayerController.cs
+++ 2 birds movement/Assets/Scripts/PlayerController.cs
@@ -12,31 +12,52 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
 
     private int jumpCount = 0;
+    private bool birdGrounded = true;
+    private float lastGroundedTime;
+    private float coyoteTime = 0.5f;
 
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+        Debug.Log(jumpCount);
 
         if (IsGrounded())
         {
-            jumpCount = 0;
+            lastGroundedTime = Time.time;
             rb.gravityScale = 1;
+            birdGrounded = true;
+        }
+        else
+        {
+            birdGrounded = false;
+            if (Time.time - lastGroundedTime > coyoteTime)
+            {
+                // Ensures jumpCount is at least 1 if coyote time has passed, preventing direct flutter jumps
+                jumpCount = Mathf.Max(1, jumpCount);
+            }
         }
 
-        if (Input.GetButtonDown("Jump") && jumpCount == 0)
+        bool canStandardJump = jumpCount == 0 && (birdGrounded || Time.time - lastGroundedTime <= coyoteTime);
+        bool canFlutterJump = jumpCount == 1;
+
+        if (Input.GetButtonDown("Jump") && canStandardJump)
         {
             rb.gravityScale = 2;
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-            Debug.Log("Spacebar pressed");
+            Debug.Log("Regular jump triggered");
+            jumpCount++;
+        }
+        else if (Input.GetButtonDown("Jump") && canFlutterJump)
+        {
+            rb.gravityScale = 4;
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            Debug.Log("Flutter jump triggered");
             jumpCount++;
         }
 
-        if (Input.GetButtonDown("Jump") && jumpCount == 1)
+        if (birdGrounded)
         {
-            rb.gravityScale = 3;
-            rb.velocity = new Vector2 (rb.velocity.x, jumpingPower);
-            Debug.Log("Flutter jump triggered");
-            jumpCount++;
+            jumpCount = 0; // Reset jumpCount when grounded
         }
 
         Flip();
