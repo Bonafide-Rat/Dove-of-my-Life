0646c9fe3daf20e2d14e13037725b203fa4a1216
Arie Maljers <ariemaljers@gmail.com>
2024-04-04 12:10:00 +1100

    Followers Working

--- Assets/Scripts/BirdFollowers.cs
+++ Assets/Scripts/BirdFollowers.cs
@@ -6,13 +6,10 @@ public class BirdFollowers : MonoBehaviour
 {
     [SerializeField] private int numFollowers;
     [SerializeField] private GameObject baseFollower;
-
-    public float savePosTime = 0.2f;
+    
     public float lerpTime = 0.5f;
-    private Rigidbody2D birdBodyRB;
-    private List<GameObject> followers = new List<GameObject>();
-
-    private List<Vector2> playerPosList = new List<Vector2>();
+    public List<GameObject> followers = new List<GameObject>();
+    private bool doMoveFollowers = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,41 +17,13 @@ public class BirdFollowers : MonoBehaviour
         {
             followers.Add(Instantiate(baseFollower, transform.position, Quaternion.identity));
         }
-        birdBodyRB = GetComponent<Rigidbody2D>();
-        
-        InvokeRepeating(nameof(addPlayerPos), 0, savePosTime);
     }
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (playerPosList.Count > 0)
+        if (followers[followers.Count -1].transform.position != transform.position)
         {
-            
-        }
-      
-        
-        if (followers[followers.Count - 1].transform.position == transform.position)
-        {
-            playerPosList.Clear();
-        }
-
-        else
-        {
-            foreach (var follower in followers)
-            {
-                for (int i = 0; i < playerPosList.Count; i++)
-                {
-                    follower.transform.position = Vector2.Lerp(follower.transform.position, playerPosList[i],lerpTime + (followers.IndexOf(follower)/100 ));
-                }
-            }
+            followers[0].transform.position = Vector2.Lerp(followers[0].transform.position,transform.position ,lerpTime);
         }
     }
-
-    private void addPlayerPos()
-    {
-        playerPosList.Add(transform.position);
-    }
-
-    
 }
