0bfeb29949efbf9ec95cd27cf5201b4231d253d8
Ivan huynh <ivan.huynh88@gmail.com>
2024-04-21 22:15:18 +1000

    Magpie Script and Sprite

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    public float verticaloffset = 3f;
+    private Vector3 offset = new Vector3(0f, 3f, -10f);
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
 
--- Assets/Scripts/Magpie.cs
+++ Assets/Scripts/Magpie.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Magpie : MonoBehaviour
+public class MoveAcrossScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 1f; // Speed of movement
+
+    private void Start()
     {
-        
+        int environmentLayer = LayerMask.NameToLayer("Environment");
+        if (environmentLayer >= 0 && environmentLayer <= 31)
+        {
+            // Ignore collisions with objects on the layer tagged as "Environment"
+            Physics.IgnoreLayerCollision(gameObject.layer, environmentLayer);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Calculate the movement vector
+        Vector3 movement = Vector3.right * speed * Time.deltaTime;
+
+        // Update the position of the GameObject
+        transform.position += movement;
     }
 }
+
+
