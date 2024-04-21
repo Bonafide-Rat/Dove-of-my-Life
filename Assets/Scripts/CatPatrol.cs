e03ecef53dde45da9aa12be69e95fa2d49d8f1c4
treblab <albertnguyen2002@hotmail.com>
2024-04-22 00:27:40 +1000

    Cat patroller script working

--- Assets/Scripts/CatPatrol.cs
+++ Assets/Scripts/CatPatrol.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class CatPatroller : MonoBehaviour
+public class CatPatrol : MonoBehaviour
 {
 
     public GameObject pointA;
     public GameObject pointB;
     private Rigidbody2D rb;
-    private Animator animator;
     private Transform currentPoint;
     [SerializeField] public float speed;
 
@@ -17,31 +16,33 @@ public class CatPatroller : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
         currentPoint = pointB.transform;
-
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Vector2 point = currentPoint.position - transform.position;
+
         if (currentPoint == pointB.transform)
         {
             rb.velocity = new Vector2(speed, 0);
 
-        } else {
-            rb.velocity = new Vector2(-speed, 0);
+            if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f)
+            {
+                flip();
+                currentPoint = pointA.transform;
+            }
         }
+        else // This is where you check if the cat is at point A
+        {
+            rb.velocity = new Vector2(-speed, 0);
 
-        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
-        {
-            flip();
-            currentPoint = pointA.transform;
-        } else
-        {
-            flip();
-            currentPoint = pointB.transform;
+            if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f)
+            {
+                flip();
+                currentPoint = pointB.transform;
+            }
         }
     }
 
