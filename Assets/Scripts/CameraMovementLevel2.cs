4a9c8abe9344d9bc373bf3fe615fc71f1d5fc399
Ivan huynh <ivan.huynh88@gmail.com>
2024-04-22 01:11:19 +1000

    camera adjustments

--- Assets/Scripts/CameraMovementLevel2.cs
+++ Assets/Scripts/CameraMovementLevel2.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class CameraFollowLevel2 : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private Vector3 offset = new Vector3(0f, 3f, -10f);
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
 
