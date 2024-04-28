129dba34d2d7e08ab1b22d50859c3399d08ef000
Arie Maljers <ariemaljers@gmail.com>
2024-04-28 23:14:31 +1000

    EndSpinColours

--- Assets/EndLineSpin.cs
+++ Assets/EndLineSpin.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,33 @@ using UnityEngine;
 public class EndLineSpin : MonoBehaviour
 {
     [SerializeField] private float spinSpeed;
+
+    private SpriteRenderer spriteRenderer;
+
+    private Color originalColour;
+
+    private bool hasChanged;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColour = spriteRenderer.color;
+        float hue, saturation, value;
+        Color.RGBToHSV(originalColour, out hue, out saturation, out value);
+        Color greyscaleColour = Color.HSVToRGB(hue, 0, value);
+        spriteRenderer.color = greyscaleColour;
+    }
+
     void Update()
     {
-        var newXRot = transform.rotation.x + spinSpeed;
-        var newyRot = transform.rotation.y + spinSpeed;
-        
         transform.Rotate(spinSpeed * Time.deltaTime, spinSpeed * Time.deltaTime,0);
+
+        if (LevelManager.TargetScore == LevelManager.CurrentScore && !hasChanged)
+        {
+            spriteRenderer.color = originalColour;
+            hasChanged = true;
+        }
     }
+    
+    
 }
