0e455495bfdf771ead4451cb7c164030bb23261e
treblab <albertnguyen2002@hotmail.com>
2024-05-05 01:18:50 +1000

    Follow path script working

---
+++ Assets/Scripts/LevelMechanicScripts/FollowPath.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FollowPath : MonoBehaviour
+{
+
+    // Array of waypoints to walk from one to the next one
+    [SerializeField]
+    private Transform[] waypoints;
+
+    // Walk speed that can be set in Inspector
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    // Index of current waypoint from which Enemy walks
+    // to the next one
+    private int waypointIndex = 0;
+
+    // Use this for initialization
+    private void Start()
+    {
+        // Set position of Enemy as position of the first waypoint
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Move Enemy
+        Move();
+    }
+
+    // Method that actually make Enemy walk
+    private void Move()
+    {
+        // If Enemy didn't reach last waypoint it can move
+        // If enemy reached last waypoint then it stops
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+            // Move Enemy from current waypoint to the next one
+            // using MoveTowards method
+            transform.position = Vector2.MoveTowards(transform.position,
+               waypoints[waypointIndex + 1].transform.position,
+               moveSpeed * Time.deltaTime);
+
+            // If Enemy reaches position of waypoint he walked towards
+            // then waypointIndex is increased by 1
+            // and Enemy starts to walk to the next waypoint
+            if (Vector2.Distance(transform.position, waypoints[waypointIndex + 1].position) < 0.5f)
+            {
+                Debug.Log("Pos reached.");
+                waypointIndex += 1;
+            }
+        }
+    }
+}
