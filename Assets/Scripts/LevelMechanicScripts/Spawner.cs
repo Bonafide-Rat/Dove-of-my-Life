22ee6a84f5219351fe1435b228f90d4747af1360
Arie Maljers <ariemaljers@gmail.com>
2024-05-06 10:50:36 +1000

    Merged Both Changes

--- Assets/Scripts/LevelMechanicScripts/Spawner.cs
+++ Assets/Scripts/LevelMechanicScripts/Spawner.cs
@@ -6,15 +6,20 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] FollowPath FollowPathObject;
     [SerializeField] Transform spawnPosition;
-
+    private bool isTriggered;
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (!other.CompareTag("Player")) return;
+        if (!FollowPathObject.gameObject.activeSelf)
         {
             //Debug.Log("Entering trigger..");
             FollowPathObject.gameObject.SetActive(true);
-            // FollowPathObject.ResetToInitialWaypoint(other.transform.position);
             FollowPathObject.ResetToInitialWaypoint(spawnPosition.position);
+        }
+        if (!isTriggered)
+        {
+            FollowPathObject.doMoveChaser = !FollowPathObject.doMoveChaser;
+            isTriggered = true;
         }
     }
 }
